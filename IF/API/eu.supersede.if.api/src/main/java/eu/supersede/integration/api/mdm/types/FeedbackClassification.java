c7f61fe55a60e5b5a9b0aa802cb7ad71cbe83084
Yosu Gorroñogoitia <jesus.gorronogoitia@atos.net>
2017-11-16 13:02:31 +0100

    Added missed class

---
+++ IF/API/eu.supersede.if.api/src/main/java/eu/supersede/integration/api/mdm/types/FeedbackClassification.java
@@ -0,0 +1,21 @@
+package eu.supersede.integration.api.mdm.types;
+
+public class FeedbackClassification {
+
+	double accuracy;
+	String classification;
+	
+	public double getAccuracy() {
+		return accuracy;
+	}
+	public void setAccuracy(double accuracy) {
+		this.accuracy = accuracy;
+	}
+	public String getClassification() {
+		return classification;
+	}
+	public void setClassification(String classification) {
+		this.classification = classification;
+	}
+
+}
