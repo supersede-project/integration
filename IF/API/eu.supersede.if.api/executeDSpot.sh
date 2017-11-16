9bfbd20a61f9c270c268687634a8de1455d6cd73
Yosu Gorroñogoitia <jesus.gorronogoitia@atos.net>
2017-11-16 14:23:29 +0100

    Fixing relative path after DSpot update

--- IF/API/eu.supersede.if.api/executeDSpot.sh
+++ IF/API/eu.supersede.if.api/executeDSpot.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-cd /home/yosu/Projects/STAMP/Git/dspot
+cd /home/yosu/Projects/STAMP/Git/dspot/dspot
 java -Dis.admin.user=admin -Dis.admin.passwd=$2pRSid# \
 -jar target/dspot-1.0.0-jar-with-dependencies.jar --verbose \
--p ../../../Supersede/Git/integration/IF/API/eu.supersede.if.api/dspot.properties \
+-p ../../../../Supersede/Git/integration/IF/API/eu.supersede.if.api/dspot.properties \
 -i 1 -t eu.supersede.integration.api.replan.controller.proxies.test.ReplanControllerProxyTest \
 -a MethodAdd \
 -s JacocoCoverageSelector
