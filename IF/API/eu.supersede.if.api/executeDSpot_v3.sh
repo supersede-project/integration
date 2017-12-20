4788c241e3310e04abe579d869f61449a51ec39c
Yosu Gorroñogoitia <jesus.gorronogoitia@atos.net>
2017-12-20 15:30:50 +0100

    Scripts to launch Descartes on IF and store results

--- IF/API/eu.supersede.if.api/executeDSpot_v3.sh
+++ IF/API/eu.supersede.if.api/executeDSpot_v3.sh
@@ -5,7 +5,8 @@ echo "Started DSpot: `date`" > $filename
 
 DSPOT_JAR=/home/yosu/Projects/STAMP/Git/dspot/dspot/target/dspot-1.0.0-jar-with-dependencies.jar
 
-dspot_opts="-i 1 -t eu.supersede.integration.api.replan.controller.proxies.test.ReplanControllerProxyTest -a MethodAdd -s JacocoCoverageSelector"
+dspot_opts="-i 2 -t eu.supersede.integration.api.replan.controller.proxies.test.ReplanControllerProxyTest -a MethodAdd -s JacocoCoverageSelector"
+#dspot_opts="-i 2 -t eu.supersede.integration.api.replan.controller.proxies.test.ReplanControllerProxyTest -a MethodAdd --verbose"
 dspot_properties="./dspot2.properties"
 echo "DSpot CMI configuration: " $dspot_opts >> $filename
 echo "DSpot properties file: " $dspot_properties >> $filename
@@ -13,7 +14,7 @@ echo "DSpot properties file: " $dspot_properties >> $filename
 nohup java -jar $DSPOT_JAR -p $dspot_properties $dspot_opts | tee -a $filename &
 
 read -p "Waiting to capture process pid" -t 5
-pid=`ps -ef | awk '$NF~"dspot" {print $2}'`
+pid=`ps -ef | grep -v grep | grep dspot-1.0.0-jar-with-dependencies.jar | awk  '{print $2}'`
 echo $pid > dspot.pid
 echo "Process pid captured"
 echo "Capturing statistics"
