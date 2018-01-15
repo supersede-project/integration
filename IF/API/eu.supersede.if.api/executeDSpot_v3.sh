f088b0d71ec1e5a354eb3bc7c686909050f6be53
Yosu Gorroñogoitia <jesus.gorronogoitia@atos.net>
2018-01-15 10:04:45 +0100

    Update script to save execution results in Git repository

--- IF/API/eu.supersede.if.api/executeDSpot_v3.sh
+++ IF/API/eu.supersede.if.api/executeDSpot_v3.sh
@@ -1,24 +1,29 @@
-date=`date '+%Y-%m-%d_%H:%M:%S'`
-filename=./dspot_results/dspot_if_$date.txt
-
-echo "Started DSpot: `date`" > $filename
-
+#Configuration
+RESULTS_DIR=/home/yosu/Projects/STAMP/Git/dspot-usecases-output/atos/supersede
 DSPOT_JAR=/home/yosu/Projects/STAMP/Git/dspot/dspot/target/dspot-1.0.0-jar-with-dependencies.jar
+DSPOT_OPTS="-i 1 -t eu.supersede.integration.api.replan.controller.proxies.test.ReplanControllerProxyTest -a MethodAdd -s JacocoCoverageSelector"
+#DSPOT_OPTS="-i 1 -t eu.supersede.integration.api.replan.controller.proxies.test.ReplanControllerProxyTest -a MethodAdd --verbose"
+DSPOT_PROPERTIES="./dspot2.properties"
 
-dspot_opts="-i 2 -t eu.supersede.integration.api.replan.controller.proxies.test.ReplanControllerProxyTest -a MethodAdd -s JacocoCoverageSelector"
-#dspot_opts="-i 2 -t eu.supersede.integration.api.replan.controller.proxies.test.ReplanControllerProxyTest -a MethodAdd --verbose"
-dspot_properties="./dspot2.properties"
-echo "DSpot CMI configuration: " $dspot_opts >> $filename
-echo "DSpot properties file: " $dspot_properties >> $filename
+date=`date '+%Y-%m-%d_%H:%M:%S'`
+RESULTS_DIR=$RESULTS_DIR/`date '+%Y-%m-%d_%H:%M:%S'`
+mkdir -p $RESULTS_DIR
+filename=$RESULTS_DIR/dspot_if_$date.txt
 
-nohup java -jar $DSPOT_JAR -p $dspot_properties $dspot_opts | tee -a $filename &
+echo "Starting DSpot: reporting in file" + $filename
+echo "Started DSpot: `date`" > $filename
+echo "DSpot CMI configuration: " $DSPOT_OPTS >> $filename
+echo "DSpot properties file: " $DSPOT_PROPERTIES >> $filename
+
+nohup java -jar $DSPOT_JAR -p $DSPOT_PROPERTIES $DSPOT_OPTS | tee -a $filename &
 
 read -p "Waiting to capture process pid" -t 5
 pid=`ps -ef | grep -v grep | grep dspot-1.0.0-jar-with-dependencies.jar | awk  '{print $2}'`
 echo $pid > dspot.pid
 echo "Process pid captured"
 echo "Capturing statistics"
-stats=./dspot_results/dspot_if_stats_$date.txt
+stats=$RESULTS_DIR/dspot_if_stats_$date.txt
 nohup ./record_dspot_stats.sh >> $stats &
-
+wait
+cp -r ./dspot-out $RESULTS_DIR/
 echo "Ended DSpot: `date`" >> $filename
