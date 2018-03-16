7d51ed781be51571a2c1ecde5f4862b4be370644
Sergi Nadal <snadal@essi.upc.edu>
2018-03-16 11:50:19 +0100

    add operator types for german feedback analysis and ontological distance

--- IF/API/eu.supersede.if.api/src/main/java/eu/supersede/integration/api/mdm/types/OperatorTypes.java
+++ IF/API/eu.supersede.if.api/src/main/java/eu/supersede/integration/api/mdm/types/OperatorTypes.java
@@ -7,13 +7,19 @@ import java.io.Serializable;
  */
 public enum OperatorTypes implements Serializable{
     VALUE("Value"),
-    FEEDBACK_CLASSIFIER_LABEL("Feedback Classifier Label"),
-    FEEDBACK_CLASSIFIER_ACCURACY("Feedback Classifier Accuracy"),
-    OVERALL_SENTIMENT("Overall Sentiment"),
-    POSITIVE_SENTIMENT("Positive Sentiment"),
-    NEGATIVE_SENTIMENT("Negative Sentiment"),
+    ENGLISH_FEEDBACK_CLASSIFIER_LABEL("English Feedback Classifier Label"),
+    ENGLISH_FEEDBACK_CLASSIFIER_ACCURACY("English Feedback Classifier Accuracy"),
+    ENGLISH_OVERALL_SENTIMENT("English Overall Sentiment"),
+    ENGLISH_POSITIVE_SENTIMENT("English Positive Sentiment"),
+    ENGLISH_NEGATIVE_SENTIMENT("English Negative Sentiment"),
     FEATURE_EXTRACTION("Extract Features (Topics)"),
-    AVERAGE_DISK_CONSUMPTION("Average feedback disk consumption today");
+    AVERAGE_DISK_CONSUMPTION("Average feedback disk consumption today"),
+    GERMAN_FEEDBACK_CLASSIFIER_LABEL("German Feedback Classifier Label"),
+    GERMAN_FEEDBACK_CLASSIFIER_ACCURACY("German Feedback Classifier Accuracy"),
+    GERMAN_OVERALL_SENTIMENT("German Overall Sentiment"),
+    GERMAN_POSITIVE_SENTIMENT("German Positive Sentiment"),
+    GERMAN_NEGATIVE_SENTIMENT("German Negative Sentiment"),
+    ONTOLOGICAL_DISTANCE("Ontological Distance");
 
     private String element;
 
