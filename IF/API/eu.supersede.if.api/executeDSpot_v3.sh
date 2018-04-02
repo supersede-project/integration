0df5b5b5886efb3e825025dfc890c68376727a52
Yosu Gorroñogoitia <jesus.gorronogoitia@atos.net>
2018-04-02 11:58:48 +0200

    Improvements in DSpot script

--- IF/API/eu.supersede.if.api/executeDSpot_v3.sh
+++ IF/API/eu.supersede.if.api/executeDSpot_v3.sh
@@ -1,7 +1,8 @@
 #Configuration
 RESULTS_DIR=/home/yosu/Projects/STAMP/Git/dspot-usecases-output/atos/supersede
-DSPOT_JAR_NAME=dspot-1.0.6-SNAPSHOT-jar-with-dependencies.jar
+DSPOT_JAR_NAME=dspot-1.0.7-SNAPSHOT-jar-with-dependencies.jar
 DSPOT_JAR=/home/yosu/Projects/STAMP/Git/dspot/dspot/target/$DSPOT_JAR_NAME
+DSPOT_OUT=./dspot-out
 
 #Selectors: JacocoCoverageSelector, CloverCoverageSelector
 DSPOT_SELECTOR=JacocoCoverageSelector
@@ -30,14 +31,20 @@ echo "DSpot CMI configuration: " $DSPOT_OPTS >> $filename
 echo "DSpot properties file: " $DSPOT_PROPERTIES >> $filename
 
 nohup java -jar $DSPOT_JAR -p $DSPOT_PROPERTIES $DSPOT_OPTS | tee -a $filename &
+pid_dpot=$!
+((pid_dpot--)) #Decremented to capture mvn command pid, otherwise it captures tee command pid
 
-read -p "Waiting to capture process pid" -t 5
-pid=`ps -ef | grep -v grep | grep $DSPOT_JAR_NAME | awk  '{print $2}'`
-echo $pid > dspot.pid
-echo "Process pid captured"
-echo "Capturing statistics"
+echo "Capturing statistics for Dspot process with pid " $pid_dpot
 stats=$RESULTS_DIR/dspot_if_stats_$date.txt
-nohup ./record_dspot_stats.sh >> $stats &
-wait
-cp -r ./dspot-out $RESULTS_DIR/
+echo "Storing statistics in " $stats
+
+./record_stats_repeat.sh $pid_dpot 180 $stats &
+pid_stats=$!
+
+wait $pid_dpot
+kill $pid_stats
+
+#copy DSpot results
+cp -r $DSPOT_OUT $RESULTS_DIR
+
 echo "Ended DSpot: `date`" >> $filename
--- IF/API/eu.supersede.if.api/executeDescartes.sh
+++ IF/API/eu.supersede.if.api/executeDescartes.sh
@@ -1,2 +1,39 @@
-#!/bin/sh
-mvn -f pom_for_descartes.xml test org.pitest:pitest-maven:mutationCoverage
+#Configuration
+DESCARTES_RESULTS_DIRECTORY=/home/yosu/Projects/STAMP/Git/descartes-usecases-output/atos/supersede
+POM_FILE=pom_for_descartes.xml
+
+date=`date '+%Y-%m-%d_%H:%M:%S'`
+RESULTS_DIR=$DESCARTES_RESULTS_DIRECTORY/`date '+%Y-%m-%d_%H:%M:%S'`
+mkdir -p $RESULTS_DIR
+#copy Descartes configuration
+cp $POM_FILE $RESULTS_DIR
+filename=$RESULTS_DIR/descartes_if_$date.txt
+
+echo "Starting Descartes: reporting in file" + $filename
+echo "Started Descartes: `date`" > $filename
+echo "Descartes pom file: " $POM_FILE >> $filename
+
+#clean and build project. 
+mvn clean package -DskipTests
+
+#Execute Descartes
+mvn -f $POM_FILE test org.pitest:pitest-maven:mutationCoverage | tee -a $filename &
+pid_descartes=$!
+((pid_descartes--)) #Decremented to capture mvn command pid, otherwise it captures tee command pid
+
+echo "Capturing statistics for descartes process with pid " $pid_descartes
+stats=$RESULTS_DIR/descartes_if_stats_$date.txt
+echo "Storing statistics in " $stats
+
+./record_stats_repeat.sh $pid_descartes 15 $stats &
+pid_stats=$!
+
+wait $pid_descartes
+kill $pid_stats
+
+echo "Ended Descartes: `date`" >> $filename
+
+#Move results to RESULTS_DIR
+report_date=`date '+%Y%m%d%H'`
+echo mv $DESCARTES_RESULTS_DIRECTORY/$report_date* $RESULTS_DIR
+mv $DESCARTES_RESULTS_DIRECTORY/$report_date* $RESULTS_DIR
