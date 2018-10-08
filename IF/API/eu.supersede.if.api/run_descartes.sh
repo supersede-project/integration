a0e8cf4e8587d681f1cafac2513f682c48175fb8
Yosu Gorroñogoitia <jesus.gorronogoitia@atos.net>
2018-10-08 16:35:07 +0200

    new scripts to compute KPI metrics

---
+++ IF/API/eu.supersede.if.api/run_descartes.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e # Any subsequent(*) commands which fail will cause the shell script to exit immediately
+
+#Cleanup
+mvn -f pom-for-stamp.xml clean package
+
+#Configuration
+DATE=`date '+%Y%m%d%H%M'`
+RESULTS_DIR=/home/yosu/Projects/STAMP/Git/descartes-usecases-output/atos/supersede/modeladapter/mutation-score/baseline/descartes/$DATE/
+OUT=./target/pit-reports/$DATE/*
+LOG=$DATE"_descartes.log"
+mkdir -p $RESULTS_DIR
+
+#Descartes Execution
+nohup time mvn -f pom_descartes.xml org.pitest:pitest-maven:mutationCoverage -DmutationEngine=descartes | tee -a $LOG &
+pid=$!
+((pid--)) #Decremented to capture mvn command pid, otherwise it captures tee command pid
+wait $pid
+
+#Collecting results
+#This will output an html report to target/pit-reports/<YYYYMMDDHHMI>.
+mv $OUT $RESULTS_DIR
+mv $LOG $RESULTS_DIR
