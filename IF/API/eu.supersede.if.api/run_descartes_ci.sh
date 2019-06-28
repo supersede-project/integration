0006421f9713b2c18c63b24965a070c01c3c325a
Yosu Gorroñogoitia <jesus.gorronogoitia@atos.net>
2019-06-28 13:22:16 +0200

    mvn configuration in script for ci

--- IF/API/eu.supersede.if.api/run_descartes_ci.sh
+++ IF/API/eu.supersede.if.api/run_descartes_ci.sh
@@ -1,4 +1,4 @@
 export MAVEN_HOME=/home/opt/maven
 export PATH=$PATH:$MAVEN_HOME/bin
-mvn -s/var/jenkins_home/settings.xml clean package -DskipTests
-mvn -s/var/jenkins_home/settings.xml -f pom_descartes.xml org.pitest:pitest-maven:mutationCoverage -DmutationEngine=descartes
+/home/opt/maven/bin/mvn -s/var/jenkins_home/settings.xml clean package -DskipTests
+/home/opt/maven/bin/mvn -s/var/jenkins_home/settings.xml -f pom_descartes.xml org.pitest:pitest-maven:mutationCoverage -DmutationEngine=descartes
