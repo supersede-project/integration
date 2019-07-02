37ed9990914b2e67d1dbe9a20d097a8b34229f05
Yosu Gorroñogoitia <jesus.gorronogoitia@atos.net>
2019-07-02 12:27:24 +0200

    Created scripts for Descartes/DSpot execution in Jenkins CI jobs

--- IF/API/eu.supersede.if.api/run_descartes_ci.sh
+++ IF/API/eu.supersede.if.api/run_descartes_ci.sh
@@ -1,4 +1,19 @@
-export MAVEN_HOME=/usr/local/maven
-export PATH=$PATH:$MAVEN_HOME/bin
-/usr/local/maven/bin/mvn -s/var/jenkins_home/settings.xml clean package -DskipTests
-/usr/local/maven/bin/mvn -s/var/jenkins_home/settings.xml -f pom_descartes.xml org.pitest:pitest-maven:mutationCoverage -DmutationEngine=descartes
+cd IF/API/eu.supersede.if.api
+#Configure MAVEN_HOME and PATH in Manage Jenkins/Configure Jenkins/Global Properties
+#Only point at a Maven installation within the /var/jenkings.home/tools folder
+#Copy IF account file
+cp /var/jenkins_home/ifaccount.properties src/test/resources/
+mvn -s/var/jenkins_home/settings.xml -f pom_descartes.xml clean package -DskipTests
+DATE=`date '+%Y%m%d%H%M'`
+DESCARTES_OUT=./target/pit-reports/$DATE
+mvn -s/var/jenkins_home/settings.xml -f pom_descartes.xml org.pitest:pitest-maven:mutationCoverage -DmutationEngine=descartes
+
+#Storing Descartes execution in descartes-usecases-output
+RESULTS_DIR=/var/jenkins_home/workspace/descartes-usecases-output/atos/supersede/if/mutation-score/baseline/descartes/$DATE/
+cp -r $DESCARTES_OUT $RESULTS_DIR
+
+cd $RESULTS_DIR
+git checkout master
+git add *
+git commit -m "Descartes execution results for Supersede IF"
+
---
+++ IF/API/eu.supersede.if.api/run_dspot_ci.sh
@@ -0,0 +1,58 @@
+#!/bin/bash -xe
+#NOTE: Copy /var/jenkins_home/settings.xml into /roo/.m2 folder of Jenkins container
+cd IF/API/eu.supersede.if.api
+mvn clean package -DskipTests
+
+#Time out:
+TIMEOUT=30000
+
+#Selectors: PitMutantScoreSelector,JacocoCoverageSelector,TakeAllSelector,ChangeDetectorSelector
+DSPOT_SELECTOR=JacocoCoverageSelector
+
+#Amplifiers: MethodAdd,MethodRemove,TestDataMutator,MethodGeneratorAmplifier,ReturnValueAmplifier,StringLiteralAmplifier,NumberLiteralAmplifier,BooleanLiteralAmplifier,CharLiteralAmplifier,AllLiteralAmplifiers,NullifierAmplifier,None
+#AllLiteralAmplifiers,MethodGeneratorAmplifier,ReturnValueAmplifier,NullifierAmplifier
+DSPOT_AMPLIFIERS=AllLiteralAmplifiers,MethodGeneratorAmplifier,ReturnValueAmplifier,NullifierAmplifier
+
+#Iterations (default=3):
+DSPOT_ITERACTIONS=3
+
+#Target Test:
+TARGET_TEST=eu.supersede.integration.api.adaptation.dashboard.proxies.test.AdaptationDashboardProxyTest
+
+#Number of amplified tests (default=200)
+MAX_TEST_AMPLIFIED=300
+
+#Budgetizer (NoBudgetizer|SimpleBudgetizer)
+BUGETIZER=NoBudgetizer
+
+DSPOT_OPTS="-Diteration=$DSPOT_ITERACTIONS -Dtest=$TARGET_TEST -Damplifiers=$DSPOT_AMPLIFIERS -Dtest-criterion=$DSPOT_SELECTOR -DtimeOut=$TIMEOUT -Dmax-test-amplified=$MAX_TEST_AMPLIFIED -Dbudgetizer=$BUGETIZER -Dclean -Dverbose"
+
+echo "DSpot configuration: " $DSPOT_OPTS
+DSPOT_PROPERTIES="./dspot.properties"
+
+#mvn help:evaluate -Dexpression=settings.localRepository | grep -v '\[INFO\]'
+DSPOT_OUT=./target/dspot/output/
+DSPOT_AMPLIFIERS_PATH=${DSPOT_AMPLIFIERS//,/-}
+RESULTS_DIR=/var/jenkins_home/workspace/dspot-usecases-output/atos/supersede/IF/DSpot
+RESULTS_DIR=$RESULTS_DIR/$DSPOT_SELECTOR/$DSPOT_AMPLIFIERS_PATH/`date '+%Y%m%d%H%M'`
+mkdir -p $RESULTS_DIR
+filename=$RESULTS_DIR/dspot.cmd
+
+echo "Starting DSpot: reporting in file" + $filename
+echo "Started DSpot: `date`" > $filename
+echo "DSpot CLI configuration: " $DSPOT_OPTS >> $filename
+echo "DSpot properties file: " $DSPOT_PROPERTIES >> $filename
+
+
+#NOTE: Copy /var/jenkins_home/settings.xml into /root/.m2 folder of Jenkins container
+mvn eu.stamp-project:dspot-maven:amplify-unit-tests -Dpath-to-properties=$DSPOT_PROPERTIES $DSPOT_OPTS
+
+#Storing DSpot execution in dspot-usecases-output
+cp -r $DSPOT_OUT $RESULTS_DIR
+cp $DSPOT_PROPERTIES $RESULTS_DIR
+
+cd $RESULTS_DIR
+git checkout master
+git add *
+git commit -m "DSpot execution results for amplification of test class(es): $TARGET_TEST"
+
