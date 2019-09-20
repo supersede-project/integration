f3e7e15a1fb076074515cb6fedc9cc94884c5c9f
Jesús Gorroñogoitia <jesus.gorronogoitia@atos.net>
2019-09-20 16:16:42 +0200

    Disabling Security Manager. Using behavioral models

--- IF/API/eu.supersede.if.api/Botsing/supersede-if-botsing/run_botsing_exception1.sh
+++ IF/API/eu.supersede.if.api/Botsing/supersede-if-botsing/run_botsing_exception1.sh
@@ -1,1 +1,1 @@
-java -jar botsing-reproduction-1.0.7.jar -Dsearch_budget=180 -Dpopulation=100 -Dtest_dir=crash-reproduction-tests -crash_log ./logs/exception1.log -target_frame 3 -projectCP ./libs
+java -jar botsing-reproduction-1.0.7.jar -Dsearch_budget=180 -Dpopulation=100 -Dtest_dir=crash-reproduction-tests -crash_log ./logs/exception1.log -target_frame 3 -projectCP ./libs -model ../models -Dp_object_pool=1.0 -Dsandbox=False
