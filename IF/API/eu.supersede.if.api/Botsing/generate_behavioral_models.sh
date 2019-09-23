bd67b0254e249cd7331940de50b9e0419008e658
Jesús Gorroñogoitia <jesus.gorronogoitia@atos.net>
2019-09-23 10:15:25 +0200

    Adding EvoSuite dependency

--- IF/API/eu.supersede.if.api/Botsing/generate_behavioral_models.sh
+++ IF/API/eu.supersede.if.api/Botsing/generate_behavioral_models.sh
@@ -1,1 +1,1 @@
-java -jar supersede-if-botsing/botsing-model-generation-1.0.7.jar -project_cp $(cat classpath.txt) -project_prefix eu.supersede.integration -out_dir models
+java -d64 -Xmx10000m -jar supersede-if-botsing/botsing-model-generation-1.0.7.jar -project_cp "$(cat classpath.txt)" -project_prefix "eu.supersede.integration" -out_dir "models"
