4e667bd88c839363a55e631f2112aa18e8f52c19
Jesús Gorroñogoitia <jesus.gorronogoitia@atos.net>
2019-09-23 11:27:02 +0200

    Fix container name usage

--- IF/API/eu.supersede.if.api/Botsing/run_image.sh
+++ IF/API/eu.supersede.if.api/Botsing/run_image.sh
@@ -1,1 +1,1 @@
-docker run --name=botsing -v ~/Git/Supersede/integration/IF/API/eu.supersede.if.api/Botsing/results:/home/results --net=host -it supersede-if-botsing
+docker run -v ~/Git/Supersede/integration/IF/API/eu.supersede.if.api/Botsing/results:/home/results --net=host -it supersede-if-botsing
