14f005c17ca5090f69b53f7ad3744583f311c45f
Jesús Gorroñogoitia <jesus.gorronogoitia@atos.net>
2019-09-30 15:54:25 +0200

    Failing DSpot amplified tests disabled

---
+++ IF/API/eu.supersede.if.api/dspot-generated-tests/third-evaluation/jacoco/eu/supersede/integration/api/tenants/test/IFTenantsManagerTest_Ampl2.java
@@ -0,0 +1,35 @@
+package eu.supersede.integration.api.tenants.test;
+
+
+import eu.supersede.integration.api.security.IFAccount;
+import eu.supersede.integration.api.tenants.IFTenantsManager;
+import java.util.List;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.springframework.util.Assert;
+import org.wso2.carbon.tenant.mgt.stub.beans.xsd.TenantInfoBean;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class IFTenantsManagerTest_Ampl2 {
+    static IFTenantsManager tm;
+
+    @BeforeClass
+    public static void setup() throws Exception {
+        String admin = IFAccount.getUser();
+        String password = IFAccount.getPassword();
+        IFTenantsManagerTest_Ampl2.tm = new IFTenantsManager(admin, password);
+    }
+
+    @Test(timeout = 30000)
+    public void getAllTenantsTest_add4() throws Exception {
+        List<TenantInfoBean> tenants = IFTenantsManagerTest_Ampl2.tm.getAllTenants();
+        Assert.notEmpty(tenants);
+        int o_getAllTenantsTest_add4__4 = tenants.size();
+        assertEquals(3, ((int) (o_getAllTenantsTest_add4__4)));
+        System.out.println((("Located " + (tenants.size())) + " tenants"));
+        assertEquals(3, ((int) (o_getAllTenantsTest_add4__4)));
+    }
+}
+
